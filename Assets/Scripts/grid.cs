071b999c0cd04958d5b8a292ce8a9d940598b964
Jockbaia <jockbaia@gmail.com>
2022-10-12 19:53:22 +0200

    spiky and shaky tiles

--- Assets/Scripts/grid.cs
+++ Assets/Scripts/grid.cs
@@ -16,6 +16,7 @@ public class grid : MonoBehaviour
     public GameObject g8;
     public GameObject g9;
     public int activeTile;
+    public int spikyTile;
     public int hoverTiles = 0;
     
     // Start is called before the first frame update
@@ -23,6 +24,9 @@ public class grid : MonoBehaviour
     {
         setActive(1);
         activeTile = 1;
+        g3.GetComponent<tile>().setSpiky();
+        spikyTile = 3;
+
     }
 
     public void setActive(int val)
@@ -106,12 +110,53 @@ public class grid : MonoBehaviour
         }
 
     }
+    
+    public void setSpiky(int val)
+    {
+        if (val == 1)
+        {
+            g1.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 2)
+        {
+            g2.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 3)
+        {
+            g3.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 4)
+        {
+            g4.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 5)
+        {
+            g5.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 6)
+        {
+            g6.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 7)
+        {
+            g7.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 8)
+        {
+            g8.GetComponent<tile>().setSpiky();
+        }
+        else if (val == 9)
+        {
+            g9.GetComponent<tile>().setSpiky();
+        }
+
+    }
 
     public void pickNewGreen(bool positive)
     {
         if(positive) ps.GetComponent<points>().add_points();
         else ps.GetComponent<points>().mistake();
-        
+
         int newGreenNumber;
 
         do
@@ -125,6 +170,35 @@ public class grid : MonoBehaviour
         if(isGreen == 1) setShaky(newGreenNumber);
         else setActive(newGreenNumber);
 
+    }
+    
+    public void pickNewSpike(bool positive)
+    {
+        if(positive) ps.GetComponent<points>().mistake();
+        
+        int newRedNumber;
+
+        do
+        {
+            newRedNumber = Random.Range(1, 10);
+        } while (newRedNumber == activeTile);
+        
+        spikyTile = newRedNumber;
+        setSpiky(newRedNumber);
+    }
+
+    public void moveSpike()
+    {
+        if (g1.GetComponent<tile>().isSpiky) g1.GetComponent<tile>().setEmpty();
+        if (g2.GetComponent<tile>().isSpiky) g2.GetComponent<tile>().setEmpty();
+        if (g3.GetComponent<tile>().isSpiky) g3.GetComponent<tile>().setEmpty();
+        if (g4.GetComponent<tile>().isSpiky) g4.GetComponent<tile>().setEmpty();
+        if (g5.GetComponent<tile>().isSpiky) g5.GetComponent<tile>().setEmpty();
+        if (g6.GetComponent<tile>().isSpiky) g6.GetComponent<tile>().setEmpty();
+        if (g7.GetComponent<tile>().isSpiky) g7.GetComponent<tile>().setEmpty();
+        if (g8.GetComponent<tile>().isSpiky) g8.GetComponent<tile>().setEmpty();
+        if (g9.GetComponent<tile>().isSpiky) g9.GetComponent<tile>().setEmpty();
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/tile.cs
+++ Assets/Scripts/tile.cs
@@ -9,7 +9,15 @@ public class tile : MonoBehaviour
     public bool isActive = false;
     public bool isShaky = false;
     public bool isHover = false;
+    public bool isSpiky = false;
     public GameObject grid;
+    public AudioSource shakySFX;
+    private bool isDangerous = false;
+
+    private void Start()
+    {
+        
+    }
 
     void OnTriggerEnter(Collider collision)
     {
@@ -25,26 +33,44 @@ public class tile : MonoBehaviour
     {
         
         // TILE IS GREEN
-        if ((isActive) && isHover && grid.GetComponent<grid>().hoverTiles == 1)
+        if ((isActive || (isShaky && !isDangerous)) && isHover && grid.GetComponent<grid>().hoverTiles == 1)
         {
             isActive = false;
+            isShaky = false;
             gameObject.GetComponent<Renderer>().material.color = Color.white;
+            grid.GetComponent<grid>().pickNewSpike(false);
             grid.GetComponent<grid>().pickNewGreen(true);
+            CancelInvoke();
         }
         
         // TILE IS YELLOW
-        else if ((isShaky) && isHover && grid.GetComponent<grid>().hoverTiles == 1)
+        else if ((isShaky && isDangerous) && isHover && grid.GetComponent<grid>().hoverTiles == 1)
         {
             isShaky = false;
             gameObject.GetComponent<Renderer>().material.color = Color.white;
+            grid.GetComponent<grid>().pickNewSpike(false);
             grid.GetComponent<grid>().pickNewGreen(false);
+            shakySFX.Stop();
+            CancelInvoke();
         }
+        
+        // TILE IS RED
+        
+        else if (isSpiky && isHover && grid.GetComponent<grid>().hoverTiles == 1)
+        {
+            isSpiky = false;
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            grid.GetComponent<grid>().pickNewSpike(true);
+        }
+        
     }
 
     private void Update()
     {
-        GetComponent<Animator>().SetBool("isActive", !isActive);
+        GetComponent<Animator>().SetBool("isActive", isActive);
         GetComponent<Animator>().SetBool("isShaky", isShaky);
+        GetComponent<Animator>().SetBool("isDangerous", isDangerous);
+        GetComponent<Animator>().SetBool("isSpiky", isSpiky);
     }
 
     void OnTriggerExit(Collider collision)
@@ -56,6 +82,7 @@ public class tile : MonoBehaviour
     public void setActive()
     {
         isShaky = false;
+        isSpiky = false;
         isActive = true;
         GetComponent<Renderer>().material.color = Color.green;
     }
@@ -63,8 +90,44 @@ public class tile : MonoBehaviour
     public void setShaky()
     {
         isActive = false;
+        isSpiky = false;
         isShaky = true;
-        GetComponent<Renderer>().material.color = Color.yellow;
+        InvokeRepeating("switchShaky", 0, 2.0f);
+        // GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void setSpiky()
+    {
+        isActive = false;
+        isSpiky = true;
+        isShaky = false;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    
+    public void setEmpty()
+    {
+        isActive = false;
+        isSpiky = false;
+        isShaky = false;
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void switchShaky()
+    {
+        if (isShaky && isDangerous)
+        {
+            shakySFX.Stop();
+            GetComponent<Renderer>().material.color = Color.green;
+            isDangerous = false;
+
+        } else if (isShaky && !isDangerous)
+        {
+            shakySFX.Play();
+            GetComponent<Renderer>().material.color = Color.yellow;
+            isDangerous = true;
+            
+        }
+
     }
     
 }
